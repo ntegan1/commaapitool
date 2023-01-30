f02fbb68f7d6dc61a7af0fcbcc16fa58e957c148
nick <ntegan1@gmail.com>
2023-01-29 23:22:34 -0500

    add upload athena method thing

--- include/commaapi.sh
+++ include/commaapi.sh
@@ -18,6 +18,15 @@ function athena_method_list_upload_queue {
   athena_message='{"method":"listUploadQueue","params":{},"jsonrpc":"2.0","id":0}'
   athena_request "${jwt}" "${dongle}" "${athena_message}"
 }
+function athena_method_upload_file_to_url {
+  jwt="${1}"
+  dongle="${2}"
+  upload_file="${3}"
+  upload_url="${4}"
+  expiry="$(date "+%s" -d tomorrow)"
+  athena_message='{"id":0,"jsonrpc":"2.0","method":"uploadFilesToUrls","params":{"files_data":[{"fn":"'"${upload_file}"'","url":"'"${upload_url}"'","headers":{"x-ms-blob-type":"BlockBlob"},"allow_cellular":false}]},"expiry":'"${expiry}"'}'
+  athena_request "${jwt}" "${dongle}" "${athena_message}"
+}
 function request {
   url=https://api.commadotai.com/
   jwt="${1}"
@@ -29,5 +38,12 @@ function request_uploaded_files_for_route {
   jwt="${1}"
   routefull="${2}" # "<dongle>|<route>"
   request "${jwt}" v1/route/"${routefull}"/files
+}
+function request_upload_url_creation {
+  jwt="${1}"
+  dongle="${2}"
+  filename="${3}"
+  expire_date_days=1
+  request "${jwt}" v1.4/"${dongle}"/upload_url/"?path=${filename}&expiry_days=${expire_date_days}"
 }
 
