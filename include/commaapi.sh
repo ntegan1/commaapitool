197443e58726c29f51f6f4fbe86ec526d157f35b
nick <ntegan1@gmail.com>
2023-01-29 22:43:44 -0500

    start listing uploaded files

--- include/commaapi.sh
+++ include/commaapi.sh
@@ -2,7 +2,7 @@ function athena_request {
   jwt="${1}"
   dongle="${2}"
   athena_message="${3}"
-  curl -H "Authorization: JWT ${jwt}" "https://athena.comma.ai/${dongle}" \
+  curl 2>/dev/null -H "Authorization: JWT ${jwt}" "https://athena.comma.ai/${dongle}" \
     -d "${athena_message}"
 }
 function athena_method_list_data_directory {
@@ -12,3 +12,16 @@ function athena_method_list_data_directory {
   athena_message='{"method":"listDataDirectory","params":{},"jsonrpc":"2.0","id":0}'
   athena_request "${jwt}" "${dongle}" "${athena_message}"
 }
+function request {
+  url=https://api.commadotai.com/
+  jwt="${1}"
+  endpoint="${2}"
+  curl 2>/dev/null -L -H "Authorization: JWT ${jwt}" \
+    "${url}/${endpoint}"
+}
+function request_uploaded_files_for_route {
+  jwt="${1}"
+  routefull="${2}" # "<dongle>|<route>"
+  request "${jwt}" v1/route/"${routefull}"/files
+}
+
