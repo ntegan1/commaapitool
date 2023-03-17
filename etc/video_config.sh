d243a4db93bd02183c835e3d36feb81da1e6a6fd
ntegan <ntegan1@gmail.com>
2023-03-17 13:43:58 -0400

    almost fq eq dq cams

---
+++ etc/video_config.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#
+#
+
+set -euo pipefail
+
+## CONSTANTS
+framew=1920
+frameh=1208
+fps=20
+#iwq=482
+#iwh=964
+#ihq=302
+#ihh=604
+
+## constants but more intended to be configured
+## by a user.
+ffmpeg_generic_opts=()
+ffmpeg_generic_opts+=(
+  "--protocol_whitelist " "concat,http,https,tls,tcp "
+)
+transcode_qp=29 # tested on amd radeon vaapi hevc_vaapi cqp prof
+transcode_frame_scale=0.6
+transcode_frame_x="$((transcode_frame_scale * framew))"
+transcode_frame_x=$((transcode_frame_x))
+transcode_frame_y="$((transcode_frame_scale * frameh))"
+transcode_frame_y=$((transcode_frame_y))
+hwaccel=vaapi # or macOS VideoToolbox 
+              # or nvdec on nvidia pascal/turing t116
+
+#
+#        "-f", "mp4",
+#        "-movflags", "empty_moov",
+
+_ffmpeg_cropscale_transcode_vaapi() {
+  in="$1"
+  out="$2"
+  scale_vaapi="${transcode_frame_w}:h=${transcode_frame_h}"
+  ffmpeg ${ffmpeg_generic_opts[@]} \
+    -hwaccel vaapi -hwaccel_output_format vaapi \
+    -r "$fps" -i "$in" \
+    -vf 'hwupload,scale_vaapi='"${scale_vaapi}"',format=nv12' \
+    -c:v hevc_vaapi \
+    -profile:v main \
+    -rc_mode CQP \
+    -qp "$transcode_qp" \
+    "$out"
+
+
+}
+ffmpeg_cropscale_transcode() { a="_ffmpeg_cropscale_transcode_'${hwaccel}'";
+  in="$1"
+  out="$2"
+  test -f "$out" && echo file alreadyexists
+  test -f "$out" && return
+  "$a" "$in" "$out"
+}
